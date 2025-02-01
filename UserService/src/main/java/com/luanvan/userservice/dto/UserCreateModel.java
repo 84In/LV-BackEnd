fe11c0b97d407f25edce462c9bf4718e64c4cd5f
84In <trungtin7166293@gmail.com>
2025-02-01 19:57:00 +0700

    tạo: tạo lại mediaservice và commonservice

--- UserService/src/main/java/com/luanvan/userservice/dto/UserCreateModel.java
+++ UserService/src/main/java/com/luanvan/userservice/dto/UserCreateModel.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 public class UserCreateModel {
     private String username;
     private String password;
-    private Boolean active;
     private String email;
     private String phone;
     private String lastName;
