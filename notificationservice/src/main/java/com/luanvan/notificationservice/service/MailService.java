a1f3ac682a0a34ad238554c08d6f1e08a60feeaa
pmss0168 <pmss0168@gmail.com>
2025-03-19 15:28:18 +0700

    cập nhât: Gửi mail hủy đơn hàng

--- notificationservice/src/main/java/com/luanvan/notificationservice/service/MailService.java
+++ notificationservice/src/main/java/com/luanvan/notificationservice/service/MailService.java
@@ -2,6 +2,7 @@ package com.luanvan.notificationservice.service;
 
 import com.luanvan.commonservice.advice.AppException;
 import com.luanvan.commonservice.advice.ErrorCode;
+import com.luanvan.commonservice.command.SendCancelledOrderMailCommand;
 import com.luanvan.commonservice.command.SendConfirmedOrderMailCommand;
 import com.luanvan.commonservice.model.response.OrderResponseModel;
 import com.luanvan.commonservice.model.response.UserResponseModel;
@@ -79,5 +80,54 @@ public class MailService {
         }
     }
 
+    @KafkaListener(topics = "send-cancelled-order-mail-topic", groupId = "mail-group")
+    public void handle(SendCancelledOrderMailCommand command) {
+        log.info("Send mail for username: {}", command.getUsername());
+        GetUserQuery userQuery = new GetUserQuery(command.getUsername());
+        UserResponseModel user = queryGateway.query(userQuery, ResponseTypes.instanceOf(UserResponseModel.class))
+                .exceptionally(ex -> {
+                    throw new AppException(ErrorCode.USER_NOT_EXISTED);
+                })
+                .join();
+        GetOrderQuery orderQuery = new GetOrderQuery(command.getOrderId());
+        OrderResponseModel order = queryGateway.query(orderQuery, ResponseTypes.instanceOf(OrderResponseModel.class))
+                .exceptionally(ex -> {
+                    throw new AppException(ErrorCode.ORDER_NOT_EXISTED);
+                })
+                .join();
+        // Prepare the context for Thymeleaf template
+        Context context = new Context();
+        context.setVariable("username", user.getUsername());
+        context.setVariable("delivery", order.getDelivery());
+        String createdAtFormattedDate = order.getCreatedAt() != null
+                ? order.getCreatedAt().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))
+                : LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+        String cancelledAtFormattedDate = order.getCreatedAt() != null
+                ? order.getUpdatedAt().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))
+                : LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+        context.setVariable("createdAt", createdAtFormattedDate);
+        context.setVariable("cancelledAt", cancelledAtFormattedDate);
+        context.setVariable("reason", command.getReason());
+        context.setVariable("orderId", order.getId());
+        context.setVariable("orderDetails", order.getOrderDetails());
+        context.setVariable("totalPrice", order.getTotalPrice());
+
+        // Process Thymeleaf template into a String
+        String htmlContent = templateEngine.process("order-confirmation", context);
+
+        // Build the email
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(user.getEmail());
+            helper.setSubject("Xác nhận đơn hàng #" + order.getId());
+            helper.setText(htmlContent, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            log.error("Failed to send email: {}", e.getMessage());
+            throw new AppException(ErrorCode.EMAIL_SENDING_FAILED);
+        }
+    }
 }
 
